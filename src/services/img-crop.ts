5ba87a5b7e6edd50ce5c8303389a6e26a80c72fc
Jonathan Kelman <kelman.jonathan@gmail.com>
2023-11-16 19:33:54 +0200

    Add img-placeholder for empty image urls in img-crop service.

--- src/services/img-crop.ts
+++ src/services/img-crop.ts
@@ -1,5 +1,7 @@
+import imgPlaceHolder from '../assets/img-placeholder.webp'
+
 const cropImage = (url: string) => {
-	if (!url) return ''
+	if (!url) return imgPlaceHolder
 	const target = 'media/'
 	const index = url.indexOf(target) + target.length
 	return url.slice(0, index) + `crop/600/400/` + url.slice(index)
