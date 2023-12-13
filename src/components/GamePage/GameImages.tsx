e4c24a56e2909afac3937b00ffa85bad51d7d5fb
Jonathan Kelman <kelman.jonathan@gmail.com>
2023-12-13 20:17:51 +0200

    Add react-photo-gallery package and implement image gallery component

--- src/components/GamePage/GameImages.tsx
+++ src/components/GamePage/GameImages.tsx
@@ -1,20 +1,50 @@
-import React from 'react'
 import { IGameDetails } from '../../interfaces/games.interface'
-import useGameDetails from '../../hooks/useGameDetails'
 import useGameScreenShots from '../../hooks/useGameScreenShots'
-import { Image } from '@chakra-ui/react'
+import { Box, Grid, GridItem, Image, SimpleGrid } from '@chakra-ui/react'
 
 interface Props {
 	gameDetails: IGameDetails
 }
 
+interface GalleryValues {
+	src: string
+	sizes?: string | string[] | undefined
+	width: number
+	height: number
+	alt?: string | undefined
+	key?: string | undefined
+}
+
 const GameImages = ({ gameDetails }: Props) => {
-	const { data, error, loading } = useGameScreenShots(gameDetails.id)
+	const { galleryArray, error, loading, loadingImages } = useGameScreenShots(gameDetails.id)
 
 	if (error) return <>error....</>
-	if (loading || !data.length) return <>Loading...</>
+	if (loading) return <>Loading...</>
+	if (loadingImages) return <>Loading. Image..</>
 
-	return <Image src={data[0].image} alt="game" />
+	return (
+		<Grid w={'700px'} h={{ base: '300px', md: '500px' }} templateRows={{ base: 'repeat(5, 1fr)', md: 'repeat(2, 1fr)' }} templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(3, 1fr)' }} gap={2}>
+			<GridItem colSpan={{ base: 1 }} display={'flex'} overflow={'hidden'} alignItems={'center'} gap={1} justifyContent={'center'}>
+				<Image src={galleryArray[0].src} objectFit={'cover'} h={'100%'} rounded={'4'} />
+			</GridItem>
+
+			<GridItem colSpan={1} display={'flex'} overflow={'hidden'} alignItems={'center'} gap={1} justifyContent={'center'}>
+				<Image src={galleryArray[1].src} objectFit={'cover'} h={'100%'} rounded={4} />
+			</GridItem>
+
+			<GridItem colSpan={1} display={'flex'} overflow={'hidden'} alignItems={'center'} gap={1} justifyContent={'center'}>
+				<Image src={galleryArray[2].src} objectFit={'cover'} h={'100%'} rounded={4} />
+			</GridItem>
+
+			<GridItem colSpan={1} display={'flex'} overflow={'hidden'} alignItems={'center'} gap={1} justifyContent={'center'} bg={'green'}>
+				<Image src={galleryArray[3].src} objectFit={'cover'} h={'100%'} rounded={4} />
+			</GridItem>
+
+			<GridItem colSpan={{ base: 1, md: 2 }} display={'flex'} justifyContent={'space-between'} alignItems={'center'} overflow={'hidden'}>
+				<Image src={galleryArray[4].src} objectFit={'cover'} h={'100%'} rounded={4} w={'100%'} />
+			</GridItem>
+		</Grid>
+	)
 }
 
 export default GameImages
--- src/hooks/useGameScreenShots.ts
+++ src/hooks/useGameScreenShots.ts
@@ -1,9 +1,34 @@
+import { useEffect, useState } from 'react'
 import { IGameScreenshots } from '../interfaces/games.interface'
 import useData from './useDataFetch'
 
+interface GalleryValues {
+	src: string
+	sizes?: string | string[] | undefined
+	width: number
+	height: number
+	alt?: string | undefined
+	key?: string | undefined
+}
+
 function useGameScreenShots(id: number) {
 	const { data, loading, error } = useData<IGameScreenshots>(`/games/${id}/screenshots`)
+	const [loadingImages, setLoadingImages] = useState(true)
+	const [galleryArray, setGalleryArray] = useState<GalleryValues[]>([])
 
-	return { data, loading, error }
+	useEffect(() => {
+		if (!data.length) return
+
+		const gallArr: GalleryValues[] = []
+		data.map((item) => {
+			const test: GalleryValues = { src: item.image, width: Math.floor(Math.random() * 2) + 1, height: Math.floor(Math.random() * 1) + 1 }
+			gallArr.push(test)
+		})
+		setGalleryArray([...gallArr])
+
+		setLoadingImages(false)
+	}, [data])
+
+	return { galleryArray, loading, error, loadingImages }
 }
 export default useGameScreenShots
