51a69dd66410617e75a3d34980a2149e796177d8
Jonathan Kelman <kelman.jonathan@gmail.com>
2023-12-16 13:31:19 +0200

    Refactor SideBar component and add UserInfo component

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,10 +1,13 @@
+import { Box, Stack } from '@chakra-ui/react'
 import GenreList from './GenreList'
+import UserInfo from './UserInfo'
 
 const SideBar = () => {
 	return (
-		<>
+		<Stack height={'100%'} p={2} spacing={10}>
+			<UserInfo />
 			<GenreList />
-		</>
+		</Stack>
 	)
 }
 
---
+++ src/components/SideBar/UserInfo.tsx
@@ -0,0 +1,28 @@
+import { Box, Button, Stack, Text, keyframes } from '@chakra-ui/react'
+import { useState } from 'react'
+
+const pulse = keyframes`
+  0% { background-color: #3b82f6; }
+  50% { background-color: #2563eb; }
+  100% { background-color: #3b82f6; }
+`
+
+const UserInfo = () => {
+	const [loading, setLoading] = useState(false)
+	return (
+		<Stack>
+			<Text fontSize={'xl'} fontWeight={'bold'}>
+				Login
+			</Text>
+			<Button bg={'red.500'} _hover={{ bg: 'red.700' }}>
+				Google
+			</Button>
+			<Button>Google</Button>
+			<Button bg={'blue.400'} animation={`${pulse} 2s infinite`}>
+				Demo Account
+			</Button>
+		</Stack>
+	)
+}
+
+export default UserInfo
