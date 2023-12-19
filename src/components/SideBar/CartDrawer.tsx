57b8f76541b6cfea05b59fc67097384e18d0cb1e
Jonathan Kelman <kelman.jonathan@gmail.com>
2023-12-19 16:41:27 +0200

    add cart drawer items

--- src/components/SideBar/CartDrawer.tsx
+++ src/components/SideBar/CartDrawer.tsx
@@ -1,10 +1,13 @@
 import { Drawer, DrawerBody, DrawerFooter, DrawerHeader, DrawerOverlay, DrawerContent, DrawerCloseButton, Button, Text, Heading, HStack, Image } from '@chakra-ui/react'
-import { useRef } from 'react'
+import { useContext, useRef } from 'react'
+import { authContext } from '../../context/authProvider'
 interface Props {
 	isCartOpen: boolean
 	onCartClose: () => void
 }
 const CartDrawer = ({ isCartOpen, onCartClose }: Props) => {
+	const { cartList } = useContext(authContext)
+
 	const btnRef = useRef<HTMLButtonElement>(null)
 	return (
 		<>
@@ -15,25 +18,21 @@ const CartDrawer = ({ isCartOpen, onCartClose }: Props) => {
 					<DrawerHeader>Cart</DrawerHeader>
 
 					<DrawerBody>
-						{/* {loading ? (
-							<>Loading</>
-						) : (
-							<>
-								{!wishList.length && <Text>Nothing to see here wtf</Text>}
-								{wishList.length && (
-									<>
-										{wishList.map((item, index) => {
-											return (
-												<HStack spacing={2} mb={2} key={index}>
-													<Image src={item.background_image} width={'50%'} />
-													<Text>{item.name} hello</Text>
-												</HStack>
-											)
-										})}
-									</>
-								)}
-							</>
-						)} */}
+						<>
+							{!cartList.length && <Text>Nothing to see here wtf</Text>}
+							{cartList.length && (
+								<>
+									{cartList.map((item, index) => {
+										return (
+											<HStack spacing={2} mb={2} key={index}>
+												<Image src={item.background_image} width={'50%'} />
+												<Text>{item.name} hello</Text>
+											</HStack>
+										)
+									})}
+								</>
+							)}
+						</>
 					</DrawerBody>
 
 					<DrawerFooter>
