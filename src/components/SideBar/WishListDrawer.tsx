06712f46bb6e4215bf932129d4ade22a95f46cb1
Jonathan Kelman <kelman.jonathan@gmail.com>
2023-12-19 16:41:27 +0200

    add cart drawer items

--- src/components/SideBar/WishListDrawer.tsx
+++ src/components/SideBar/WishListDrawer.tsx
@@ -14,29 +14,6 @@ const WishListDrawer = ({ isWishListOpen, onWishListClose }: Props) => {
 
 	const btnRef = useRef<HTMLButtonElement>(null)
 
-	const [loading, setLoading] = useState(false)
-
-	const fetchGameList = async () => {
-		// if (!user) return
-		// setLoading(true)
-		// const res = await fetchList(user)
-		// if (!res) {
-		// 	setLoading(false)
-		// 	return console.log('eroror')
-		// }
-		// setWishList(res)
-		// setLoading(false)
-	}
-
-	useEffect(() => {
-		fetchGameList()
-	}, [])
-
-	useEffect(() => {
-		if (!user) return
-		// setWishList([...(user.wishList || [])])
-	}, [user])
-
 	return (
 		<>
 			<Drawer isOpen={isWishListOpen} placement="right" onClose={onWishListClose} finalFocusRef={btnRef} size={'lg'}>
@@ -46,25 +23,21 @@ const WishListDrawer = ({ isWishListOpen, onWishListClose }: Props) => {
 					<DrawerHeader>Wish List</DrawerHeader>
 
 					<DrawerBody>
-						{loading ? (
-							<>Loading</>
-						) : (
-							<>
-								{!wishList.length && <Text>Nothing to see here wtf</Text>}
-								{wishList.length && (
-									<>
-										{wishList.map((item, index) => {
-											return (
-												<HStack spacing={2} mb={2} key={index}>
-													<Image src={item.background_image} width={'50%'} />
-													<Text>{item.name} hello</Text>
-												</HStack>
-											)
-										})}
-									</>
-								)}
-							</>
-						)}
+						<>
+							{!wishList.length && <Text>Nothing to see here wtf</Text>}
+							{wishList.length && (
+								<>
+									{wishList.map((item, index) => {
+										return (
+											<HStack spacing={2} mb={2} key={index}>
+												<Image src={item.background_image} width={'50%'} />
+												<Text>{item.name} hello</Text>
+											</HStack>
+										)
+									})}
+								</>
+							)}
+						</>
 					</DrawerBody>
 
 					<DrawerFooter>
