b37cf50c785c3acf85723c54889c19c55dc3d4b3
Jonathan Kelman <kelman.jonathan@gmail.com>
2023-12-20 10:03:32 +0200

    created generic drawers

---
+++ src/utils/DrawerGeneric.tsx
@@ -0,0 +1,55 @@
+import { Drawer, DrawerBody, DrawerFooter, DrawerHeader, DrawerOverlay, DrawerContent, DrawerCloseButton, Button, Text, HStack, Image, Stack, Tag } from '@chakra-ui/react'
+import { IGameDetails } from '../interfaces/games.interface'
+import GamePlatforms from '../components/HomePage/GamePlatforms'
+import WishListButton from './WishListButton'
+import CartButton from './CartButton'
+import { useContext, useEffect, useState } from 'react'
+import { authContext } from '../context/authProvider'
+interface Iparams {
+	openDrawer: string | null
+}
+
+const DrawerGeneric = ({ openDrawer }: Iparams) => {
+	const { wishList, cartList } = useContext(authContext)
+	const [listType, setListType] = useState(wishList)
+
+	useEffect(() => {
+		if (openDrawer === 'cartList') setListType(cartList)
+	}, [openDrawer])
+
+	return (
+		<DrawerBody>
+			<>
+				{!listType.length && <Text>Nothing to see here wtf</Text>}
+				{listType.length && (
+					<>
+						{listType.map((game, index) => {
+							return (
+								<Stack direction="row" spacing={2} mb={2} key={index} bg={'blackAlpha.700'} borderRadius={'lg'} shadow={'base'} alignItems={'stretch'}>
+									<Image src={game.background_image} width={'154px'} height={'159px'} objectFit={'cover'} />
+									<Stack>
+										<a>
+											<Text fontWeight={'bold'} fontSize={'lg'} mt={'10px'}>
+												{game.name}
+											</Text>
+										</a>
+										<Tag w={'100px'} justifyContent={'center'}>
+											$ 14.99
+										</Tag>
+										<GamePlatforms platforms={game.parent_platforms.map((p) => p.platform)} />
+										<HStack>
+											<WishListButton game={game} />
+											<CartButton game={game} />
+										</HStack>
+									</Stack>
+								</Stack>
+							)
+						})}
+					</>
+				)}
+			</>
+		</DrawerBody>
+	)
+}
+
+export default DrawerGeneric
