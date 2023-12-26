5bdd66769bc40056fa96c6fdbdf04624b2775fb5
Jonathan Kelman <kelman.jonathan@gmail.com>
2023-12-26 23:00:54 +0200

    added nav to search response

--- src/components/Header/SearchResponse.tsx
+++ src/components/Header/SearchResponse.tsx
@@ -2,29 +2,43 @@ import { Box, Stack, Text, VStack } from '@chakra-ui/react'
 import OutsideClickHandler from 'react-outside-click-handler'
 
 import { IGame } from '../../interfaces/games.interface'
-import { useEffect, useState } from 'react'
 import useSearch from '../../hooks/useSearch'
+import { useNavigate } from 'react-router-dom'
 
 interface Props {
 	dataResponse: IGame[]
 	showResults: boolean
-
 	setShowResults: React.Dispatch<React.SetStateAction<boolean>>
 }
 
 const SearchResponse = ({ dataResponse, showResults, setShowResults }: Props) => {
+	const nav = useNavigate()
 	const handleClickOutside = () => {
 		setShowResults(false)
 	}
-
+	const navigateGamePage = (gameId: number) => {
+		nav(`/game/${gameId}`, { state: { id: gameId } })
+	}
 	return (
 		<>
 			{showResults && dataResponse?.length && (
 				<OutsideClickHandler onOutsideClick={handleClickOutside}>
-					<Box bg={'blue.700'} position={'absolute'} w={'100%'} top={'40px'} h={'300px'} zIndex={1} overflow={'scroll'}>
+					<Box bg={'blackAlpha.900'} position={'absolute'} w={'80%'} top={'40px'} h={'300px'} zIndex={1} overflow={'scroll'}>
 						<VStack>
 							{dataResponse.map((game) => {
-								return <Text key={game.id}>{game.name}</Text>
+								return (
+									<Box
+										p={2}
+										_hover={{ bg: 'blue.500' }}
+										w={'100%'}
+										onClick={() => {
+											navigateGamePage(game.id)
+											handleClickOutside()
+										}}
+									>
+										<Text key={game.id}>{game.name}</Text>
+									</Box>
+								)
 							})}
 						</VStack>
 					</Box>
