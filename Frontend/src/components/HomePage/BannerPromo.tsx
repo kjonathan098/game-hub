cb9d1026b182eafed8d352c0d6ad63822ce3122b
Jonathan Kelman <kelman.jonathan@gmail.com>
2024-01-02 12:26:08 +0200

    feature : added wishList btn in banner & refactro banner media to be game type

--- Frontend/src/components/HomePage/BannerPromo.tsx
+++ Frontend/src/components/HomePage/BannerPromo.tsx
@@ -1,4 +1,4 @@
-import { Box, HStack, Image, Stack, Text, Progress, Fade } from '@chakra-ui/react'
+import { Box, HStack, Image, Stack, Text, Progress, Fade, Center } from '@chakra-ui/react'
 import fifa from '../../assets/HomePageCarouselPromo/fifa24.avif'
 import alanWake from '../../assets/HomePageCarouselPromo/alanWake.avif'
 import lotr from '../../assets/HomePageCarouselPromo/lotr.avif'
@@ -15,7 +15,7 @@ const BannerPromo = () => {
 	const [progress, setProgress] = useState(0)
 
 	const selectGame = (num: number) => {
-		setCurrentGame(num)
+		setCurrentGame(num + 1)
 		setProgress(0) // reset progress when a game is clicked
 	}
 	useEffect(() => {
@@ -44,28 +44,30 @@ const BannerPromo = () => {
 
 	return (
 		<Stack direction={'row'}>
-			<Box w={'75%'}>
+			<Center w={'75%'}>
 				{bannerMedia.map((game, index) => {
 					return (
 						<Fade in={currentGame === index + 1}>
-							<Box as="span" position={'relative'}>
-								<Image src={game.background} objectFit={'cover'} rounded={'lg'} key={index} display={currentGame === index + 1 ? 'block' : 'none'} />
+							<Box as="span" position={'relative'} display={currentGame === index + 1 ? 'block' : 'none'} h={'100%'}>
+								<Image src={game.background} objectFit={'cover'} rounded={'lg'} key={index} />
 								<Stack position={'absolute'} bottom={'100px'} maxW={'500px'} spacing={'5'} p={4}>
 									<Image src={game.titleLogo} width={'40%'} />
 									<Text fontWeight={'black'}>{game.description}</Text>
-									<HStack>{/* <WishListButton /> */}</HStack>
+									<HStack zIndex={2}>
+										<WishListButton game={game} size={'md'} />
+									</HStack>
 								</Stack>
 								<Box as="span" position={'absolute'} bottom={'0px'} width={'100%'} height={'100%'} opacity={'0.6'} bgGradient="linear(to-r, rgba(11, 11, 11, 0) 0%, rgba(11, 11, 11, 0) 50%, #0B0B0B 100%)" />
 							</Box>
 						</Fade>
 					)
 				})}
-			</Box>
+			</Center>
 			<Box width={'25%'}>
 				<Stack p={4} display={'flex'} justifyContent={'space-between'} height={'100%'}>
 					{bannerMedia.map((game, index) => {
 						return (
-							<HStack key={index} onClick={() => selectGame(index)} position="relative" className="game-item" rounded={'lg'} bg={currentGame === index + 1 ? 'blackAlpha.400' : ''}>
+							<HStack key={index} onClick={() => selectGame(index)} position="relative" className="game-item" rounded={'lg'} bg={currentGame === index + 1 ? 'blackAlpha.400' : ''} _hover={{ cursor: 'pointer', bg: 'blackAlpha.300' }}>
 								<Image src={game.background} objectFit={'cover'} h={'100%'} w={'100%'} rounded={'lg'} height={'100px'} width={'80px'} />
 								<Text fontSize={'xl'} fontWeight={'bold'}>
 									{game.name}
