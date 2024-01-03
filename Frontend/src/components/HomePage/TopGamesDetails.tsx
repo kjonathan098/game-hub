0dc8b0e8406d6dbc9f94cdd47062dca080a6ee56
Jonathan Kelman <kelman.jonathan@gmail.com>
2024-01-03 23:38:28 +0200

    styling: added responsiveness to banner Promo HP

--- Frontend/src/components/HomePage/TopGamesDetails.tsx
+++ Frontend/src/components/HomePage/TopGamesDetails.tsx
@@ -3,46 +3,40 @@ import { topSellers } from './BannerPromoMedia'
 import CartButton from '../../utils/CartButton'
 import WishListButton from '../../utils/WishListButton'
 import YouTube, { YouTubeProps } from 'react-youtube'
+import { useEffect, useState } from 'react'
 
 type Props = {
 	gameSelected: number
 }
 
 const TopGamesDetails = ({ gameSelected }: Props) => {
-	const opts: YouTubeProps['opts'] = {
+	const optObj = {
 		height: '455',
 		width: '100%',
 		playerVars: {
 			autoplay: 1,
 			mute: 1,
 			loop: 1,
-			playlist: topSellers[gameSelected - 1].youtubeId,
+			playlist: topSellers[gameSelected - 1].youtubeId!,
 		},
 	}
+	const [videoId, setVideoId] = useState<string>(topSellers[gameSelected - 1].youtubeId!)
+	const [opts, setOpts] = useState<YouTubeProps['opts']>(optObj)
+
+	useEffect(() => {
+		const newVideoId = topSellers[gameSelected - 1].youtubeId!
+		setVideoId(newVideoId)
+		setOpts({
+			...optObj,
+			playersVar: { ...optObj.playerVars, playlist: newVideoId },
+		})
+	}, [gameSelected])
+
 	return (
 		<Box height={'100%'} width={'100%'} bg={'green.400'} position={'relative'}>
-			<YouTube videoId={topSellers[gameSelected - 1].youtubeId} opts={opts} />
+			<YouTube key={videoId} videoId={videoId} opts={opts} />
 		</Box>
 	)
 }
 
 export default TopGamesDetails
-
-// {/* <Grid templateColumns={{ base: '1fr ', lg: '2fr 1fr' }} gap={2} height={'100%'}>
-// <GridItem>
-// 	<YouTube videoId="GNC-8lQsvrY" />;{/* <Image src={topSellers[gameSelected - 1].background_image} width={'100%'} objectFit={'cover'} height={'100%'} /> */}
-// </GridItem>
-// <GridItem justifyContent={'space-between'}>
-// 	<Stack spacing={'4'}>
-// 		<Image src={topSellers[gameSelected - 1].titleLogo} />
-// 		<Text>{topSellers[gameSelected - 1].description}</Text>
-// 		<Stack spacing={'4'}>
-// 			<Tag colorScheme="teal" width={'fit-content'}>
-// 				$30
-// 			</Tag>
-// 			<WishListButton game={topSellers[1]} />
-// 			<CartButton game={topSellers[1]} />
-// 		</Stack>
-// 	</Stack>
-// </GridItem>
-// </Grid> */}
