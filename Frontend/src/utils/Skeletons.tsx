7ab2fbcc4308708a778c40393b578f3f79fd886e
Jonathan Kelman <kelman.jonathan@gmail.com>
2024-01-07 13:08:06 +0200

    styling: addded skeleton struct to game page details

---
+++ Frontend/src/utils/Skeletons.tsx
@@ -0,0 +1,62 @@
+import { Box, Button, Center, Grid, GridItem, HStack, Skeleton, Stack, Tag, Text, useBreakpointValue } from '@chakra-ui/react'
+
+export const GamesSwiperSkeleton = () => {
+	return (
+		<Stack mt={3}>
+			<Box w={{ base: '100vw', lg: '1200px' }} pl={{ base: 2 }}>
+				<HStack>
+					{Array(5)
+						.fill(0)
+						.map((_, i) => (
+							<Stack width={{ base: '173px', lg: '230px' }} spacing={3} key={i}>
+								<Skeleton height={{ base: '230px', lg: '300px' }} width={{ base: '173px', lg: 'auto' }} rounded={'md'} mr={3} />
+								<Skeleton height="20px" width="80%" />
+								<Skeleton height="20px" width="60%" />
+							</Stack>
+						))}
+				</HStack>
+			</Box>
+		</Stack>
+	)
+}
+
+export const GamePageSkeleton = () => {
+	const slideViews = useBreakpointValue({ base: 1.5, md: 3.5, lg: 4.5 })
+	const imageWidth = useBreakpointValue({ base: '200px', md: '125px', lg: '100px' })
+	const imageHeight = useBreakpointValue({ base: '110px', md: '125px', lg: '70px' })
+	return (
+		<Box>
+			<Grid templateColumns={{ base: '1fr', lg: '2fr 1fr' }} gap={2} w={{ base: '100vw', lg: '1200px' }} h={'90vh'}>
+				<GridItem minW={{ base: '100%', lg: '900px' }}>
+					<Skeleton h={'500px'} w={'900px'} mb={3} />
+					<Center w={'900px'}>
+						<HStack spacing={3}>
+							{Array(5)
+								.fill(0)
+								.map((_, index) => {
+									return <Skeleton key={index} width={imageWidth} h={imageHeight} />
+								})}
+						</HStack>
+					</Center>
+				</GridItem>
+				<GridItem p={2}>
+					<Box height={'100%'}>
+						<Center h={'100%'}>
+							<Stack justifyContent={'space-around'} h={'100%'} spacing={5}>
+								<Center>
+									<Skeleton width={'200px'} h={'50px'} />
+								</Center>
+								<Skeleton w={'100px'} h={'30px'} />
+								<Skeleton size="lg" w={'200px'} h={'48px'} />
+								<Skeleton size="lg" w={'200px'} h={'48px'} />
+								<Skeleton size="lg" w={'200px'} h={'32px'} />
+								<Skeleton size="lg" w={'200px'} h={'32px'} />
+								<Skeleton size="lg" w={'200px'} h={'32px'} />
+							</Stack>
+						</Center>
+					</Box>
+				</GridItem>
+			</Grid>
+		</Box>
+	)
+}
